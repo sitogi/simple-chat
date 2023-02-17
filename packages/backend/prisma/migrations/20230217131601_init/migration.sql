605189c4cdd6a58674ca7f44c64c001f7b72d212
sitogi <takano162@gmail.com>
2023-02-17 22:17:29 +0900

    refactor: clean migration files

--- packages/backend/prisma/migrations/20230217131601_init/migration.sql
+++ packages/backend/prisma/migrations/20230217131601_init/migration.sql
@@ -1,16 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Tokens` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `Tokens`;
-
--- DropTable
-DROP TABLE `User`;
-
 -- CreateTable
 CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
